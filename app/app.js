da42bf3a614f8289fc0d387b0daf3509ca05e0d9
Matt Carter <m@ttcarter.com>
2015-05-19 15:43:21 +1000

    Added Angular-xeditable to project Library tags are now editable

--- app/app.js
+++ app/app.js
@@ -2,5 +2,17 @@ var app = angular.module('app', [
 	'colorpicker.module',
 	'ng-collection-assistant',
 	'ngResource',
-	'ui.router'
+	'ui.router',
+	'xeditable'
 ]);
+
+// Config: editable-text {{{
+app.run(function(editableThemes, editableOptions) {
+	editableThemes.bs3.inputClass = 'input-sm';
+	editableThemes.bs3.buttonsClass = 'btn-sm';
+	editableThemes.bs3.submitTpl = '<button type="submit" class="btn btn-success"><span></span></button>';
+	editableThemes.bs3.cancelTpl = '<button type="button" class="btn btn-danger" ng-click="$form.$cancel()"><span></span></button>';
+	editableOptions.icon_set = 'font-awesome';
+	editableOptions.theme = 'bs3';
+});
+// }}}
