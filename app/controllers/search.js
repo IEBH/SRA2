8b661b91447ac3a7b88789ed6f3917b46c2c2147
Matt Carter <m@ttcarter.com>
2015-06-04 12:32:36 +1000

    Added search spinner Added advanced search options + method selection

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,14 +1,37 @@
 app.controller('SearchController', function($scope, Searches) {
+	$scope.searching = false;
 	$scope.query = '';
-	$scope.method = 'pubmed';
 	$scope.response = null;
 
 	$scope.submit = function() {
+		$scope.searching = true;
 		Searches.get({
-			method: $scope.method,
+			method: $scope.method.id,
 			q: $scope.query,
-		}).$promise.then(function(data) {
-			$scope.response = data;
-		});
+		}).$promise
+			.then(function(data) {
+				$scope.response = data;
+			})
+			.finally(function(data) {
+				$scope.searching = false;
+			});
 	};
+
+	// Advanced options {{{
+	$scope.advanced = false;
+	$scope.toggleAdvanced = function() {
+		$scope.advanced = !$scope.advanced;
+	};
+	// }}}
+
+	// Methods {{{
+	$scope.methods = [
+		{id: 'pubmed', title: 'PubMed'}
+	];
+	$scope.method = _.find($scope.methods, {id: 'pubmed'});
+
+	$scope.setMethod = function(method) {
+		$scope.method = method;
+	};
+	// }}}
 });
