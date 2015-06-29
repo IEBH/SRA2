52e34b67cc91453e520b590740720bcc94177f98
Matt Carter <m@ttcarter.com>
2015-06-30 09:51:51 +1000

    Added missing fields into libraries model BUGFIX: Wrong field used in journal request subject

--- cron/request.js
+++ cron/request.js
@@ -94,7 +94,7 @@ module.exports = function(finish, task) {
 						to: config.library.request.email.to,
 						cc: config.library.request.email.cc,
 						bcc: config.library.request.email.bcc,
-						subject: 'Journal request - ' + (task.settings.user.title || 'Unknown title'),
+						subject: 'Journal request - ' + (ref.title || 'Unknown title'),
 						body: this.html,
 						bodyType: 'html',
 					}).send(next);
--- models/libraries.js
+++ models/libraries.js
@@ -8,6 +8,17 @@ var schema = new mongoose.Schema({
 	dedupeStatus: {type: String, enum: ['none', 'processing', 'review'], default: 'none'},
 	debug: {type: String, enum: ['active', 'inactive'], default: 'inactive'},
 	title: {type: String},
+	files: [{
+		name: {type: String},
+		size: {type: Number},
+		url: {type: String}
+	}],
+	screening: {
+		weightings: [{
+			keyword: {type: String},
+			weight: {type: Number}
+		}],
+	},
 });
 
 module.exports = mongoose.model(name, schema);
