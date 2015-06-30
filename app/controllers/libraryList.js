25bed16fd118a525444260af41f5684dbb50975a
Yujun Wang <wangyujun.bond@gmail.com>
2015-06-30 16:07:52 +1000

    Added functions for handling new email registry and library owners

--- app/controllers/libraryList.js
+++ app/controllers/libraryList.js
@@ -1,4 +1,4 @@
-app.controller('libraryListController', function($scope, Libraries, References) {
+app.controller('libraryListController', function($scope, Libraries, References, Users) {
 	$scope.libraries = null;
 
 	$scope.$watch('libraryAllowNew + libraries', function() {
@@ -37,9 +37,63 @@ app.controller('libraryListController', function($scope, Libraries, References) 
 	// }}}
 
 	// Share emails inline edit {{{
+	$scope.library = null;
 	$scope.editShare = function(library) {
 		$scope.library = library;
+		console.log("$scope.library:",$scope.library);
 		$('#modal-shareLib').modal('show');
 	};
 	// }}}
+
+	// Validate email address {{{
+	$scope.validateEmail = function (email) {
+		var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+		return re.test(email);
+	}
+	// }}}
+
+	$scope.emailList = null;
+	$scope.shareLib = function(library) {
+		//Step1 Create new users with emails
+		//Step2 Add new users to library's owners list
+		var emailArray = $scope.emailList.split(";");
+		console.log("emailArray:", emailArray);
+
+		_.forEach(emailArray, function(v, key) {
+			if ($scope.validateEmail(v.trim())){ //email is legal
+				//Check whether the email exists
+				Users.query({email: v.trim()}).$promise.then(function(user){
+					if (user[0]){ //Add user to library's owners list
+						if (_.indexOf(library.owners, user[0]._id) < 0){ //user is in owner's list
+							library.owners.push(user[0]._id);
+							Libraries.save(
+								{id: library._id},
+								_.pick(library.owners, ['owners'])
+							);
+						}
+					}else{
+						//Register user
+						var signup= {};
+						signup.username = v.trim();
+						signup.email = signup.username;
+						signup.name = 'New User';
+						signup.password = 'qwaszx'
+
+						Users.signup({}, signup).$promise.then(function(user){
+							console.log('new user:', user);
+							//Add new user to library's owners list
+							library.owners.push(user._id);
+							console.log("library.owners:",library.owners);
+							Libraries.save(
+								{id: library._id},
+								_.pick(library.owners, ['owners'])
+							);
+						});
+					}
+				});
+			}
+		});
+
+		//Step3 Email login details
+	};
 });
