5b944219e8f9e6f5b64ee76388b24d6072e4eac8
Yujun Wang <wangyujun.bond@gmail.com>
2015-07-02 09:03:07 +1000

    Further simplified the textselect directive

--- app/directives/textselect.js
+++ app/directives/textselect.js
@@ -5,13 +5,8 @@ app.directive('textselect', function() {
 		restrict: 'EA',
 		controller: function($scope, $element) {
 			$scope.getSelected = function() {
-
-				var handler = [
-					window.getSelection ? window.getSelection().toString() : null,
-					document.getSelection ? document.getSelection).toString() : null,
-					document.selection && document.selection.createRange() ? selection.text : null
-				].find();
-
+				var handler = window.getSelection ? (window.getSelection()).toString() : null;
+					
 				return handler;
 			}
 
@@ -22,7 +17,6 @@ app.directive('textselect', function() {
 			elem
 				.on('mouseup', function() {
 					var selection = $scope.getSelected();
-					console.log('selection:', JSON.stringify(selection));
 					if (selection) {
 						alert(selection);
 					}
