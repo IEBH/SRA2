73ba2fbb7c37d190b9be2b3823ba3a75e95e62a9
Matt Carter <m@ttcarter.com>
2015-07-03 12:34:54 +1000

    NPM update BUGFIX: Missing server URL in production config

--- config/production.js
+++ config/production.js
@@ -3,6 +3,7 @@ var colors = require('colors');
 console.log(colors.bold.red('RUNNING IN PRODUCTION MODE'));
 
 module.exports = {
+	url: 'http://beta.crebp-sra.com.au',
 	gulp: {
 		debugJS: false,
 		minifyJS: true,
