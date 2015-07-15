6f8a268cc9a06fdb2a867ddcbbe5b309994547a9
Matt Carter <m@ttcarter.com>
2015-07-15 17:55:23 +1000

    BUGFIX: Wrong URL for production site

--- config/production.js
+++ config/production.js
@@ -3,7 +3,7 @@ var colors = require('colors');
 console.log(colors.bold.red('RUNNING IN PRODUCTION MODE'));
 
 module.exports = {
-	url: 'http://beta.crebp-sra.com.au',
+	url: 'http://beta.crebp-sra.com',
 	gulp: {
 		debugJS: false,
 		minifyJS: true,
