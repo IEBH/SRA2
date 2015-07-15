b54f76d5e8383dc5086029fa90cdff29691d5e37
Matt Carter <m@ttcarter.com>
2015-07-15 17:09:56 +1000

    Added comparison directive to make comparing rows easier

--- app/models/referenceTags.js
+++ app/models/referenceTags.js
@@ -1,5 +1,7 @@
 app.factory('ReferenceTags', function($resource) {
 	return $resource('/api/referenceTags/:id', {}, {
-		create: {url: '/api/referenceTags', 'method': 'POST'}
+		get: {url: '/api/referenceTags/:id', 'method': 'GET'},
+		getCached: {url: '/api/referenceTags/:id', 'method': 'GET', cache: true},
+		create: {url: '/api/referenceTags', 'method': 'POST'},
 	});
 });
