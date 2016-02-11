337328a84799c8b7c13dabeece16d9abdb645b5b
Matt Carter <m@ttcarter.com>
2016-02-11 15:38:07 +1000

    Security patch to only allow access to references if they are either requesting a specific one OR are querying by library

--- controllers/references.js
+++ controllers/references.js
@@ -1,3 +1,14 @@
 var References = require('../models/references');
 
-restify.serve(app, References);
+restify.serve(app, References, {
+	preRead: function(req, res, next) {
+		if (req.method.toUpperCase() == 'GET') {
+			if (req.params.id || req.query.id) return next(); // Read a specific ID - allowed
+			if (req.query.library) return next(); // Requesting from a specific library - allowed
+			// If we got here the requester is probably trying to spy on another library
+			return res.send('Either ID or library is required to query references').status(403).end();
+		} else {
+			return next();
+		}
+	},
+});
