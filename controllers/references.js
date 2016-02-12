b99c4e584a4da06d1343e8726bb81b791458a99e
Matt Carter <m@ttcarter.com>
2016-02-12 15:54:05 +1000

    BUGFIX: Reference security not allowing anything though with ERM-queryizer modeule installed

--- controllers/references.js
+++ controllers/references.js
@@ -2,9 +2,11 @@ var References = require('../models/references');
 
 restify.serve(app, References, {
 	preRead: function(req, res, next) {
+		var q = req.query.query ? JSON.parse(req.query.query) : {};
+
 		if (req.method.toUpperCase() == 'GET') {
-			if (req.params.id || req.query.id) return next(); // Read a specific ID - allowed
-			if (req.query.library) return next(); // Requesting from a specific library - allowed
+			if (req.params.id || q.id) return next(); // Read a specific ID - allowed
+			if (q.library) return next(); // Requesting from a specific library - allowed
 			// If we got here the requester is probably trying to spy on another library
 			return res.send('Either ID or library is required to query references').status(403).end();
 		} else {
