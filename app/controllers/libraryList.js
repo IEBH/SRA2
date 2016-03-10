2d971be3a0f13a6406abc5f04054a57b3da283a0
Matt Carter <m@ttcarter.com>
2016-03-10 15:32:18 +1000

    Tidied up reference edit page Reference edit page now displays loading spinner when libraries are still loading

--- app/controllers/libraryList.js
+++ app/controllers/libraryList.js
@@ -1,4 +1,5 @@
 app.controller('libraryListController', function($scope, Libraries, References, Users, $location) {
+	$scope.loading = true;
 	$scope.libraries = null;
 
 	$scope.$watch('libraryAllowNew + libraries', function() {
@@ -18,20 +19,24 @@ app.controller('libraryListController', function($scope, Libraries, References, 
 	// Data refresher {{{
 	$scope.refresh = function() {
 		if (!$scope.user) return;
-		Libraries.query({status: 'active', owners: $scope.user._id}).$promise.then(function(data) {
-			$scope.libraries = data
-				// Decorators {{{
-				// .referenceCount {{{
-				.map(function(library) {
-					library.referenceCount = null;
-					References.count({library: library._id}).$promise.then(function(countData) {
-						library.referenceCount = countData.count;
+		Libraries.query({status: 'active', owners: $scope.user._id}).$promise
+			.then(function(data) {
+				$scope.libraries = data
+					// Decorators {{{
+					// .referenceCount {{{
+					.map(function(library) {
+						library.referenceCount = null;
+						References.count({library: library._id}).$promise.then(function(countData) {
+							library.referenceCount = countData.count;
+						});
+						return library;
 					});
-					return library;
-				});
-				// }}}
-				// }}}
-		});
+					// }}}
+					// }}}
+			})
+			.finally(function() {
+				$scope.loading = false;
+			});
 	};
 	$scope.$evalAsync($scope.refresh);
 	// }}}
