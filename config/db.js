fa6cf40794996a141e59affbe9e18aebea8b0c37
Matt Carter <m@ttcarter.com>
2016-03-21 10:03:58 +1000

    Database connection system now uses Monoxide

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 /**
-* Connect to the Mongoose DB and provide 'mongoose' and 'db' as globals
+* Connect to the main storage database
 * Requires that config/index.js has already been loaded into global.config
 */
-global.mongoose = require('mongoose');
+var colors = require('colors');
+var monoxide = require('monoxide');
+
 console.log('Connecting to Mongo', config.mongo.uri);
-mongoose.connect(config.mongo.uri);
-global.db = global.mongoose.connection;
-db.on('error', console.error.bind(console, 'DB connection error:'));
+monoxide.connect(config.mongo.uri)
+	.on('error', function(err) {
+		console.log(colors.red('DB CONNECTION', err));
+	});
