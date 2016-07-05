9e54ed6b2e471908d5b8f115e6ea6df6c47e3810
Matt Carter <m@ttcarter.com>
2016-07-05 10:13:03 +1000

    Primary studies interface for Replicant is now a collapse - also shows count of selected studies

--- app/controllers/replicantOptions.js
+++ app/controllers/replicantOptions.js
@@ -54,5 +54,38 @@ app.controller('replicantOptionsController', function($scope, $location, $q, $st
 		console.log($scope.options);
 	};
 
+	// .counts - count of studies, comparisons, subcomparisons {{{
+	$scope.counts = {
+		studies: undefined,
+		comparisons: undefined,
+		subComparisons: undefined,
+		text: '',
+	};
+	$scope.$watch('revman', function() {
+		if (!$scope.revman) return; // Object not yet loaded
+		$scope.counts.studies = 0;
+		$scope.counts.comparisons = 0;
+		$scope.counts.subComparisons = 0;
+
+		$scope.revman.forEach(function(study) {
+			if (study.selected) $scope.counts.studies++;
+			study.comparisons.forEach(function(comparison) {
+				if (comparison.selected) $scope.counts.comparisons++;
+				comparison.subComparisons.forEach(function(subComparison) {
+					if (subComparison.selected) $scope.counts.subComparisons++;
+				});
+			});
+		});
+
+		$scope.counts.text = _([
+			$scope.counts.studies + ' studies',
+			$scope.counts.comparisons + ' comparisons',
+			$scope.counts.subComparisons + ' sub-comparisons',
+		])
+			.filter()
+			.join(', ');
+	}, true);
+	// }}}
+
 	$scope.$evalAsync($scope.refresh);
 });
