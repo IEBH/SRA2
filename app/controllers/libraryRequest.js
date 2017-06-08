2af89d0fdaaa8cfe361543dfd2a3e39730be87d9
Matt Carter <m@ttcarter.com>
2017-06-08 10:20:29 +1000

    Changed copyright + terms notice in journal request + added enforced agreement conditions

--- app/controllers/libraryRequest.js
+++ app/controllers/libraryRequest.js
@@ -1,7 +1,18 @@
-app.controller('libraryRequestController', function($scope, $location, Tasks) {
+app.controller('libraryRequestController', function($scope, $location, $notification, Tasks) {
 	$scope.ccUser = true;
 
+	$scope.agree = {
+		copyright: false,
+		terms: false,
+	};
+
 	$scope.submit = function() {
+		if (!$scope.agree.copyright) {
+			return $notification.error('You must agree to the copyright notice');
+		} else if (!$scope.agree.terms) {
+			return $notification.error('You must agree to the terms');
+		}
+
 		Tasks.fromLibrary({id: $scope.library._id, worker: 'request'}, {settings: {
 			user: _.pick($scope.user, ['email', 'name', 'title', 'libraryNo', 'faculty', 'position']),
 			ccUser: $scope.ccUser,
