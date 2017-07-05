c69be2620b5c269c80fa73b40da2289ff68c0a09
Matt Carter <m@ttcarter.com>
2017-07-05 11:11:29 +1000

    BUGFIX: Can now click links in embedded documents and they open in new tabs

--- app/controllers/helpView.js
+++ app/controllers/helpView.js
@@ -1,4 +1,4 @@
-app.controller('helpViewController', function($scope, $location, $rootScope, $sce, $stateParams) {
+app.controller('helpViewController', function($scope, $http, $location, $rootScope, $stateParams) {
 	// $scope.topics - inherited from parent
 
 	$scope.topic;
@@ -6,12 +6,25 @@ app.controller('helpViewController', function($scope, $location, $rootScope, $sc
 	$scope.topic = $scope.topics.find(i => i.id == $stateParams.topic);
 	if (!$scope.topic) return $location.path('/help');
 
-	$scope.topic.url = $sce.trustAsResourceUrl($scope.topic.url);
-
 	// Deal with breadcrumbs {{{
 	$rootScope.$broadcast('setBreadcrumb', [
 		{url: '/help', title: 'Help'},
 	]);
 	$rootScope.$broadcast('setTitle', $scope.topic.title);
+	// }}}
+
+	// Load topic from URL and rewrite <a> targets {{{
+	// We have to manually rewrite <a> links as Google Docs seemingly provides no way to do this
+	// Any link being clicked in an IFrame will try to redirect within the IFrame and refuse due to content policy
+	// @see https://stackoverflow.com/q/28343653/1295040
+	$scope.$watch('topic.url', ()=> {
+		$.get($scope.topic.url, html => {
+			$("#google-doc-iframe").attr("srcdoc", html);
+			setTimeout(function() {
+				$("#google-doc-iframe").contents().find('a[href^="http://"]').attr("target", "_blank");
+				$("#google-doc-iframe").contents().find('a[href^="https://"]').attr("target", "_blank");
+			}, 1000);
+		});
+	});
 	// }}}
 });
