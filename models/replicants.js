1978be83bb6b6e54c7958b93edf92dedb84b22f8
Matt Carter <m@ttcarter.com>
2017-08-30 12:31:50 +1000

    Converted replicant model to Monoxide

--- models/replicants.js
+++ models/replicants.js
@@ -1,13 +1,11 @@
-var name = 'replicants';
-var schema = new mongoose.Schema({
-	id: mongoose.Schema.ObjectId,
-	created: {type: Date, default: Date.now},
-	owner: {type: mongoose.Schema.ObjectId, ref: 'users'},
-	randomSeed: {type: Number, default: 1}, // The random seed to use (regenerated on each call to /api/replicant/:id/generate?randomize=true)
-	title: {type: String},
-	revman: {type: mongoose.Schema.Types.Mixed}, // The nested RevMan object extracted via the revman module
-	grammar: {type: String}, // The basename of the gammar file to use
-	primary: {type: Array}, // Array of IDs used within the revman object that are primary studies
-});
+var monoxide = require('monoxide');
 
-module.exports = mongoose.model(name, schema);
+module.exports = monoxide.schema('replicants', {
+	created: {type: 'date', default: Date.now},
+	owner: {type: 'pointer', ref: 'users'},
+	randomSeed: {type: 'number', default: 1}, // The random seed to use (regenerated on each call to /api/replicant/:id/generate?randomize=true)
+	title: {type: 'string'},
+	revman: {type: 'object'}, // The nested RevMan object extracted via the revman module
+	grammar: {type: 'string'}, // The basename of the gammar file to use
+	primary: {type: 'array'}, // Array of IDs used within the revman object that are primary studies
+});
