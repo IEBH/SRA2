b91fda8c3f082302b0f5f1646194179998a6bf67
Connor Forbes <connorforbes25@gmail.com>
2021-07-14 10:43:09 +1000

    feat: Update polyglot to v4

--- app/controllers/polyglot.js
+++ app/controllers/polyglot.js
@@ -1,7 +1,7 @@
 app.controller('PolyglotController', ['$scope','$location', '$sce', function($scope, $location, $sce) {
     var params = $location.search();
     var token = params.token;
-    var polyglotUrl = "https://iebh.github.io/sra-polyglot";
+    var polyglotUrl = "https://polyglot.sr-accelerator.com";
     if(token) {
       polyglotUrl = polyglotUrl.concat("?token=" + token);
     }
