66dfcc816962ba42802b027bb76d07558403e1f3
Connor Forbes <connorforbes25@gmail.com>
2021-09-02 15:27:48 +1000

    fix: Watch for user email changes

--- app/controllers/searchrefinery.js
+++ app/controllers/searchrefinery.js
@@ -1,9 +1,11 @@
 app.controller('searchrefineryController', ['$scope', '$sce', function($scope, $sce) {
-    var username = $scope.user.email;
-    var searchrefineryUrl = "https://searchrefinery.sr-accelerator.com";
-    if(username) {
-      searchrefineryUrl = searchrefineryUrl.concat("?username=" + username);
-    }
-    // console.log(searchrefineryUrl);
-    $scope.searchrefineryUrl = $sce.trustAsResourceUrl(searchrefineryUrl);
-  }]);
+	$scope.$watchGroup(['user.email'], function() {
+		var username = $scope.user.email;
+		var searchrefineryUrl = "https://searchrefinery.sr-accelerator.com";
+		if(username) {
+			searchrefineryUrl = searchrefineryUrl.concat("?username=" + username);
+		}
+		console.log(searchrefineryUrl);
+		$scope.searchrefineryUrl = $sce.trustAsResourceUrl(searchrefineryUrl);
+	});
+}]);
