a7b79099a70b6120c2ced0ef9ea09c1358450bf5
Matt Carter <m@ttcarter.com>
2023-08-30 13:34:01 +1000

    fix: More terafarmer resources

--- app/controllers/terafarmer.js
+++ app/controllers/terafarmer.js
@@ -1,6 +1,4 @@
 app.controller('TerafarmerController', ['$scope','$location', '$sce', function($scope, $location, $sce) {
-    var params = $location.search();
-    var token = params.token;
     var terafarmerUrl = "https://terafarmer.tera-tools.com";
     $scope.terafarmerUrl = $sce.trustAsResourceUrl(terafarmerUrl);
   }]);
